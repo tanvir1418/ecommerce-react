feaeeebb69d95f3668b733e0e45bc3410f29465c
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2023-11-01 10:57:00 +0600

    Project Basic Template To SideBar JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Navbar, Sidebar, Footer } from './components';
+import { Home, Products, SingleProduct, About, Cart, Error, Checkout, PrivateRoute } from './pages';
+
 function App() {
-  return <h4>comfy sloth starter</h4>;
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/products/:id'>
+          <SingleProduct />
+        </Route>
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route path='*'>
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
