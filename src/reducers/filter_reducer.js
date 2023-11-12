9c622e3c39b816ea88489e4425bf6a1924f7e7e7
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2023-11-13 01:04:43 +0600

    Up to Filtering Colors

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,7 +11,18 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
-    return { ...state, all_products: [...action.payload], filtered_products: [...action.payload] };
+    let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
+    return {
+      ...state,
+      all_products: [...action.payload],
+      filtered_products: [...action.payload],
+      filters: {
+        ...state.filters,
+        max_price: maxPrice,
+        price: maxPrice,
+      },
+    };
   }
   if (action.type === SET_GRIDVIEW) {
     return { ...state, grid_view: true };
@@ -51,6 +62,14 @@ const filter_reducer = (state, action) => {
     }
     return { ...state, filtered_products: tempProducts };
   }
+  if (action.type === UPDATE_FILTERS) {
+    const { name, value } = action.payload;
+    return { ...state, filters: { ...state.filters, [name]: value } };
+  }
+  if (action.type === FILTER_PRODUCTS) {
+    return { ...state };
+  }
+
   // return state;
   throw new Error(`No Matching "${action.type}" - action type`);
 };
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -5,4 +5,27 @@ export const formatPrice = (number) => {
   }).format(number / 100);
 };
 
-export const getUniqueValues = () => {};
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if (type === 'colors') {
+    unique = unique.flat();
+  }
+  return ['all', ...new Set(unique)];
+};
+
+export const convertToTitleCase = (str) => {
+  // const exceptions = ['of', 'the', 'and', 'to'];
+  const exceptions = [];
+
+  if (!str) {
+    return '';
+  }
+
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word, i) => {
+      return exceptions.includes(word) && i != 0 ? word : word.charAt(0).toUpperCase().concat(word.substr(1));
+    })
+    .join(' ');
+};
