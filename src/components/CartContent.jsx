f83355a5e961c7095fefa07450ccc5a0696f5181
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2023-11-16 00:05:49 +0600

    Cart Page Implemented

--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -6,8 +6,28 @@ import CartItem from './CartItem';
 import CartTotals from './CartTotals';
 
 const CartContent = () => {
-  return <h4>cart content </h4>;
+  const { cart, clearCart } = useCartContext();
+
+  return (
+    <Wrapper className='section section-center'>
+      <CartColumns />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <hr />
+      <div className='link-container'>
+        <Link to='/products' className='link-btn'>
+          Continue Shopping
+        </Link>
+        <button type='button' className='link-btn clear-btn' onClick={clearCart}>
+          Clear Shopping Cart
+        </button>
+      </div>
+      <CartTotals />
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.section`
   .link-container {
     display: flex;
@@ -30,4 +50,5 @@ const Wrapper = styled.section`
     background: var(--clr-black);
   }
 `;
+
 export default CartContent;
